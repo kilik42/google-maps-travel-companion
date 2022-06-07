d3ab8e1376354224e1e02c1a22315b75b076f7f0
kilik42 <marvinevins@yahoo.com>
2022-06-07 14:04:06 -0500

    working on app.js page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
+import {CssBaseline, Grid} from '@material-ui/core';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 import PlaceDetails from './components/PlaceDetails/PlaceDetails';
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+        <CssBaseline />
+       <Header />
+       <Grid container spacing={3} style={{width: '100%'}}>
+         <Grid item xs={12} md={4}>
+
+         </Grid>
+       </Grid>
+
+
+    </>
   );
 }
 
