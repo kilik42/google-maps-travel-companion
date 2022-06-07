2a94afbd2398912156cb6b7f9f56257d8978cd24
kilik42 <marvinevins@yahoo.com>
2022-06-07 13:55:11 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+import PlaceDetails from './components/PlaceDetails/PlaceDetails';
 function App() {
   return (
     <div className="App">
